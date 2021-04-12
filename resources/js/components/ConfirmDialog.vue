ad6cb1d0d1411c5e8326aacdf31e1402ce93215a
Bram Adl <thenamesbram@gmail.com>
2021-04-12 20:07:49 +0700

    Hotfix: Update totte bag bug 2

--- resources/js/components/ConfirmDialog.vue
+++ resources/js/components/ConfirmDialog.vue
@@ -129,7 +129,7 @@ export default {
     color() {
       if (
         !this.$route.query.tag.startsWith("tshirt") ||
-        !this.$route.query.tag.startsWith('totte_bag') &&
+        !this.$route.query.tag.startsWith('totte_bag') ||
         !this.$route.query.tag.startsWith("bundle_")
       ) {
         return "-";
